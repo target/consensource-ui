2dd76029db0446524656270da05d7d0c8bc3adbf
Patrick-Erichsen <Patrick.Erichsen@target.com>
2020-03-09 13:28:29 -0500

    Create script to compile protos to TS
    Signed-off-by: Patrick-Erichsen <Patrick.Erichsen@target.com>

---
+++ scripts/compile_protobufs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Compiling .proto files..."
+
+js_file="./src/protobuf/protos.js"
+ts_file="./src/protobuf/protos.d.ts"
+
+npx pbjs -t static-module -w commonjs -o $js_file $(ls -d "$PWD"/protos/*)
+npx pbts -o $ts_file $js_file
+
+echo "Successfully compiled all .proto files"
