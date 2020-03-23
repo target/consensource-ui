b8d041631927c66844140e21d13b62503d6208d5
Patrick-Erichsen <Patrick.Erichsen@target.com>
2020-03-23 09:49:09 -0500

    Remove test file
    Signed-off-by: Patrick-Erichsen <Patrick.Erichsen@target.com>

--- src/services/test.ts
+++
@@ -1,9 +0,0 @@
-import { CertificateRegistryPayload, AccreditCertifyingBodyAction } from 'App/protobuf';
-
-class PublicKey {
-    constructor() {
-        if (this.constructor === PublicKey) {
-            throw new TypeError('Cannot construct abstract class');
-        }
-    }
-}
