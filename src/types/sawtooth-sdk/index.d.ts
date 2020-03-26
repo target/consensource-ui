6f4007925e4a9f394d0c97e514a9cd1b31daf5df
Trevor McDonald <trevor.mcdonald@target.com>
2020-03-26 12:35:26 -0500

    Bring in sawtooth types, to be removed once available from npm

--- src/types/sawtooth-sdk/index.d.ts
+++ src/types/sawtooth-sdk/index.d.ts
@@ -1,14 +1,28 @@
-// Type definitions for Sawooth SDK
-// TypeScript: 3.8
+// Type definitions for sawtooth-sdk 1.1
 // Project: https://www.hyperledger.org/projects/sawtooth, https://github.com/hyperledger/sawtooth-core
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/sawtooth-sdk
-// Definitions by:
-//  - Patrick Erichsen <https://github.com/Patrick-Erichsen>
-//  - Trevor McDonald  <https://github.com/trevormcdonald>
+// Definitions by: Patrick Erichsen <https://github.com/Patrick-Erichsen>
+//                 Trevor McDonald  <https://github.com/trevormcdonald>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/** Signing  */
-export import signing = require('./signing');
-export import core = require('./signing/core');
-export import secp256k1 = require('./signing/secp256k1');
+// Copyright (c) 2020 Target Brands, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 export as namespace sawtooth;
+
+import * as signing from './signing';
+import * as protobuf from './protobuf';
+import * as exceptions from './processor/exceptions';
+import * as processor from './processor';
+
+export { signing, protobuf, exceptions, processor };
--- src/types/sawtooth-sdk/signing/secp256k1.d.ts
+++ src/types/sawtooth-sdk/signing/secp256k1.d.ts
@@ -1,69 +1,116 @@
-// Type definitions for Sawooth SDK
-// TypeScript: 3.8
-// Project: https://www.hyperledger.org/projects/sawtooth, https://github.com/hyperledger/sawtooth-core
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/sawtooth-sdk
-// Definitions by:
-//  - Patrick Erichsen <https://github.com/Patrick-Erichsen>
-//  - Trevor McDonald  <https://github.com/trevormcdonald>
+// Copyright (c) 2020 Target Brands, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import core = require('./core');
-import signing = require('./');
+/// <reference types="node" />
 
-declare module secp256k1 {
-    class Secp256k1PrivateKey extends core.PrivateKey {
-        constructor(privateKeyBytes: Buffer);
+import { PrivateKey, PublicKey, Context } from './core';
+import { message } from './';
 
-        /** The bytes of the public key */
-        privateKeyBytes: Buffer;
+/**
+ * @param privateKeyBytes - the bytes of the private key
+ */
+export class Secp256k1PrivateKey extends PrivateKey {
+    constructor(privateKeyBytes: Buffer);
 
-        /** Return a string of the algorithm name */
-        getAlgorithmName(): string;
+    /** The bytes of the public key */
+    privateKeyBytes: Buffer;
 
-        /** Returns the private key bytes in a Buffer. */
-        asBytes(): Buffer;
+    /**
+     * @return name of the algorithm
+     */
+    getAlgorithmName(): string;
 
-        /** Creates a private key from a hex encode set of bytes. */
-        static fromHex(privateKeyHex: string): Secp256k1PrivateKey;
+    /**
+     * @return the key in bytes
+     */
+    asBytes(): Buffer;
 
-        /** Generate a new random private key */
-        static newRandom(): Secp256k1PrivateKey;
-    }
+    /**
+     * Creates a private key from a hex encode set of bytes.
+     *
+     * @param privateKeyHex - the key in hex
+     * @return a private key instance
+     * @throws if the private key is not valid
+     */
+    static fromHex(privateKeyHex: string): PrivateKey;
 
-    class Secp256k1PublicKey extends core.PublicKey {
-        constructor(publicKeyBytes: Buffer);
-
-        /** The bytes of the public key */
-        publicKeyBytes: Buffer;
-
-        /** Return a string of the algorithm name */
-        getAlgorithmName(): string;
-
-        /** Returns the public key bytes in a Buffer. */
-        asBytes(): Buffer;
-
-        /** Creates a public key from a hex encode set of bytes. */
-        static fromHex(publicKeyHex: string): Secp256k1PublicKey;
-
-        /** Generate a new random public key */
-        static newRandom(): Secp256k1PublicKey;
-    }
-
-    class Secp256k1Context extends core.Context {
-        /** Return a string of the algorithm name */
-        getAlgorithmName(): string;
-
-        /** Verify that a provided message and Secp256k1PublicKey match a given message */
-        verify(signature: string, message: signing.message, publicKey: Secp256k1PublicKey): boolean;
-
-        /** Sign a given message with the provided Secp256k1PublicKey */
-        sign(message: signing.message, privateKey: Secp256k1PublicKey): string;
-
-        /** Return a new Secp256k1PublicKey instance from a Secp256k1PrivateKey instance*/
-        getPublicKey(privateKey: Secp256k1PrivateKey): Secp256k1PublicKey;
-
-        /** Generate a new Secp256k1PrivateKey */
-        newRandomPrivateKey(): Secp256k1PrivateKey;
-    }
+    /**
+     * @return generates a random PrivateKey
+     *
+     */
+    static newRandom(): PrivateKey;
 }
 
-export = secp256k1;
+/**
+ * @param publicKeyBytes - the bytes of the public key
+ */
+export class Secp256k1PublicKey extends PublicKey {
+    constructor(publicKeyBytes: Buffer);
+
+    /** The bytes of the public key */
+    publicKeyBytes: Buffer;
+
+    getAlgorithmName(): string;
+
+    /**
+     * @return the key in bytes
+     */
+    asBytes(): Buffer;
+
+    /**
+     * Creates a public key from a hex encode set of bytes.
+     *
+     * @param publicKeyHex - the key in hex
+     * @return a public key instance
+     * @throws if the public key is not valid
+     */
+    static fromHex(publicKeyHex: string): PublicKey;
+
+    /** Generate a new random public key */
+    static newRandom(): PublicKey;
+}
+
+export class Secp256k1Context extends Context {
+    getAlgorithmName(): string;
+
+    /**
+     * Verify that the signature of a given messages was signed by the proved Secp256k1PublicKey
+     *
+     * @param signature
+     * @param message
+     * @param publicKey
+     * @return result of verifying if the signature matches the Secp256k1PublicKey
+     */
+    verify(signature: string, message: message, publicKey: Secp256k1PublicKey): boolean;
+
+    /**
+     * Sign a given message with the provided PublicKey
+     *
+     * @param message
+     * @param privateKey
+     * @return string of the signed message
+     */
+    sign(message: message, privateKey: Secp256k1PrivateKey): string;
+
+    /**
+     * Return a computed Secp256k1PublicKey from a Secp256k1PrivateKey.
+     * @param privateKey
+     */
+    getPublicKey(privateKey: Secp256k1PrivateKey): Secp256k1PublicKey;
+
+    /**
+     * Generate a new random private key, based on the Secp256k1 algorithm.
+     */
+    newRandomPrivateKey(): Secp256k1PrivateKey;
+}
