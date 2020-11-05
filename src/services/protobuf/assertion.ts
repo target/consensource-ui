6865279d361657261260e2cb8d135920ea12bed6
Trevor McDonald <trevor.mcdonald@target.com>
2020-11-05 15:49:29 -0600

    Add assertion transfer service and interface
    Signed-off-by: Trevor McDonald <trevor.mcdonald@target.com>

--- src/services/protobuf/assertion.ts
+++ src/services/protobuf/assertion.ts
@@ -1,12 +1,16 @@
 import {
   ConsenSourceNamespaces,
+  FAMILY_NAMESPACE,
+  RESERVED_NAMESPACE,
   createStateAddress,
   getAgentStateAddress,
 } from 'services/addressing';
 import {
   AssertAction,
+  TransferAssertionAction,
   ICertificateRegistryPayload,
   IAssertAction,
+  ITransferAssertionAction,
 } from 'services/protobuf/compiled';
 import {
   createTransaction,
@@ -14,13 +18,20 @@ import {
   encodePayload,
   ACTIONS,
 } from 'services/protobuf/transaction';
+
 import { CreateOrgActionStrict } from 'services/protobuf/organization';
 
 export interface IAssertActionStrict extends IAssertAction {
-  assertion_id: string;
-  new_factory?: {
-    factory: CreateOrgActionStrict;
-  };
+  assertion_id: NonNullable<IAssertAction['assertion_id']>;
+  new_factory?: { factory: CreateOrgActionStrict };
+}
+
+export interface ITransferAssertionActionStrict
+  extends ITransferAssertionAction {
+  assertion_id: NonNullable<ITransferAssertionAction['assertion_id']>;
+  new_owner_public_key: NonNullable<
+    ITransferAssertionAction['new_owner_public_key']
+  >;
 }
 
 /**
@@ -29,7 +40,9 @@ export interface IAssertActionStrict extends IAssertAction {
  */
 export type AssertActionStrict = IAssertActionStrict & AssertAction;
 
-export function getAssertionStateAddress(assertion_id: string) {
+export function getAssertionStateAddress(
+  assertion_id: AssertActionStrict['assertion_id'],
+) {
   return createStateAddress(ConsenSourceNamespaces.ASSERTION, assertion_id);
 }
 
@@ -61,6 +74,70 @@ export function createAssertionActionTransaction(
 
     addresses.push(orgStateAddress);
   }
+
+  const payloadInfo: PayloadInfo = {
+    payloadBytes,
+    inputs: addresses,
+    outputs: addresses,
+  };
+
+  return createTransaction(payloadInfo, signer);
+}
+
+/**
+ * Enforce that an `TransferAssertionAction` has the minimum
+ * required fields defined in `ITransferAssertionActionStrict`
+ */
+export type TransferAssertionActionStrict = ITransferAssertionActionStrict &
+  TransferAssertionAction;
+
+export function createTransferAssertionAction(
+  transfer: ITransferAssertionActionStrict,
+) {
+  return TransferAssertionAction.create(transfer);
+}
+
+/**
+ * List of required input/output addresses for possible state objects
+ * that are looked up or written to during transaction validation:
+ *   - The agent that is initiating the transfer
+ *   - The organization prefix
+ *   - The certificate prefix
+ *   - The standard prefix
+ *   - The certifying body that is granting the certificate
+ *   - The assertion that is being transferred
+ */
+function getAddresses(
+  { assertion_id }: TransferAssertionActionStrict,
+  signer: sawtooth.signing.Signer,
+) {
+  const agent = getAgentStateAddress(signer);
+
+  const org_prefix =
+    FAMILY_NAMESPACE + RESERVED_NAMESPACE + ConsenSourceNamespaces.ORGANIZATION;
+
+  const cert_prefix =
+    FAMILY_NAMESPACE + RESERVED_NAMESPACE + ConsenSourceNamespaces.CERTIFICATE;
+
+  const standard_prefix =
+    FAMILY_NAMESPACE + RESERVED_NAMESPACE + ConsenSourceNamespaces.STANDARD;
+
+  const assertion = getAssertionStateAddress(assertion_id);
+
+  return [agent, org_prefix, cert_prefix, standard_prefix, assertion];
+}
+
+export function createTransferAssertionActionTransaction(
+  transfer_assertion_action: TransferAssertionActionStrict,
+  signer: sawtooth.signing.Signer,
+): sawtooth.protobuf.Transaction {
+  const payload: ICertificateRegistryPayload = {
+    action: ACTIONS.TRANSFER_ASSERTION,
+    transfer_assertion_action,
+  };
+  const payloadBytes = encodePayload(payload);
+
+  const addresses = getAddresses(transfer_assertion_action, signer);
 
   const payloadInfo: PayloadInfo = {
     payloadBytes,
