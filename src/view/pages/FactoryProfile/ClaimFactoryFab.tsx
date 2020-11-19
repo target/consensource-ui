70bfe9b41981146667adfcf5a9c5b35bad71c48d
Trevor McDonald <trevor.mcdonald@target.com>
2020-11-19 16:04:59 -0600

    Change claim fab to use make/create styles instead of with

--- src/view/pages/FactoryProfile/ClaimFactoryFab.tsx
+++ src/view/pages/FactoryProfile/ClaimFactoryFab.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import {
-  Fab,
-  createStyles,
-  WithStyles,
-  withStyles,
-  Theme,
-} from '@material-ui/core';
+import { Fab, makeStyles, createStyles, Theme } from '@material-ui/core';
 import { VerifiedUserOutlined } from '@material-ui/icons';
 import { FactoryResData } from 'services/api';
 import { TransferFactoryDialog } from 'view/modals';
 import { useAuth } from 'services/hooks';
 
-const styles = (theme: Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    closeButton: {
+    fab: {
       margin: 0,
       top: 'auto',
       right: 20,
@@ -24,43 +18,43 @@ const styles = (theme: Theme) =>
     extendedIcon: {
       marginRight: theme.spacing(1),
     },
-  });
+  }),
+);
 
-export interface ClaimFactoryFabProps extends WithStyles<typeof styles> {
+export interface ClaimFactoryFabProps {
   factory: FactoryResData;
 }
 
-export const ClaimFactoryFab = withStyles(styles)(
-  ({ factory, classes }: ClaimFactoryFabProps) => {
-    const { signer } = useAuth();
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => {
-      setOpen(true);
-    };
-    const handleClose = () => {
-      setOpen(false);
-    };
+export const ClaimFactoryFab = ({ factory }: ClaimFactoryFabProps) => {
+  const classes = useStyles();
+  const { signer } = useAuth();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-      <>
-        <Fab
-          color="secondary"
-          variant="extended"
-          aria-label="claim"
-          className={classes.closeButton}
-          onClick={handleOpen}
-          disabled={!signer}
-        >
-          <VerifiedUserOutlined className={classes.extendedIcon} />
-          Claim this factory
-        </Fab>
+  return (
+    <>
+      <Fab
+        color="secondary"
+        variant="extended"
+        aria-label="claim"
+        className={classes.fab}
+        onClick={handleOpen}
+        disabled={!signer}
+      >
+        <VerifiedUserOutlined className={classes.extendedIcon} />
+        Claim this factory
+      </Fab>
 
-        <TransferFactoryDialog
-          open={open}
-          handleClose={handleClose}
-          factory={factory}
-        />
-      </>
-    );
-  },
-);
+      <TransferFactoryDialog
+        open={open}
+        handleClose={handleClose}
+        factory={factory}
+      />
+    </>
+  );
+};
