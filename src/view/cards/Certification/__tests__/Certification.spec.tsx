d27c4ac1a42f5a3e3430ec3eda025a12fa98cb07
MargiMerline <Margaret.Merline@target.com>
2020-11-24 09:00:48 -0600

    Got tests working for the cert card.

--- src/view/cards/Certification/__tests__/Certification.spec.tsx
+++ src/view/cards/Certification/__tests__/Certification.spec.tsx
@@ -1,22 +1,25 @@
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { render, fireEvent, screen } from 'utils/testing';
+import { render, screen } from 'utils/testing';
 import { mockCerts } from 'services/api/__mocks__';
 import { CertificationCard } from '../Certification';
 
 describe('<Certificates />', () => {
-  const certificate = { ...mockCerts, data: [] as any };
+  const { data: CertificateData } = mockCerts;
   it('renders', () => {
     const { container } = render(
-      <CertificationCard certification={certificate.data} />,
+      <CertificationCard certification={CertificateData[0]} />,
     );
     expect(container).toMatchSnapshot();
   });
-  it('open up the cert page when more info link is clicked', () => {
-    render(<CertificationCard certification={certificate.data} />);
-    userEvent.click(screen.getByTitle('More Info'));
-    /*expect(document.querySelector('More Info')?.getAttribute('href')).toBe(
-      `{/certifications/${id}}`,
-    );*/
+  it('Checks that the logic for expiration date is working and checks that the more info button works', () => {
+    render(<CertificationCard certification={CertificateData[0]} />);
+    expect(screen.getByText('Expired on 8/18/2020'));
+    expect(screen.getByText('Claimed'));
+    userEvent.click(screen.getByText('More Info'));
+    expect(screen.getByRole('button')).toHaveAttribute(
+      'href',
+      `/certifications/${CertificateData[0].id}`,
+    );
   });
 });
